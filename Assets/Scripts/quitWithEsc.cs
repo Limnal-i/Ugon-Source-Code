c621a63616d161675235758a754adf1727142e05
Limnal <kahanovlior@yahoo.com>
2024-12-31 15:28:36 +0000

    Ready
    lots of stuff

---
+++ Assets/Scripts/quitWithEsc.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitWithEsc : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
