a620dd7d719332bb0b0367f008f044d680a5d917
Limnal <kahanovlior@yahoo.com>
2025-01-31 11:16:10 +0000

    Button Solution
    Code by stackoverflow user Programmer:
    https://stackoverflow.com/a/37906512

---
+++ Assets/Scripts/spriteButton.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class spriteButton : MonoBehaviour, IPointerClickHandler,
+                                  IPointerDownHandler, IPointerEnterHandler,
+                                  IPointerUpHandler, IPointerExitHandler
+{
+    // Code coppied from https://stackoverflow.com/a/37906512 made by stackoverflow user Programmer
+
+    void Start()
+    {
+        //Attach Physics2DRaycaster to the Camera
+        Camera.main.gameObject.AddComponent<Physics2DRaycaster>();
+
+        addEventSystem();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+
+    //Add Event System to the Camera
+    void addEventSystem()
+    {
+        GameObject eventSystem = null;
+        GameObject tempObj = GameObject.Find("EventSystem");
+        if (tempObj == null)
+        {
+            eventSystem = new GameObject("EventSystem");
+            eventSystem.AddComponent<EventSystem>();
+            eventSystem.AddComponent<StandaloneInputModule>();
+        }
+        else
+        {
+            if ((tempObj.GetComponent<EventSystem>()) == null)
+            {
+                tempObj.AddComponent<EventSystem>();
+            }
+
+            if ((tempObj.GetComponent<StandaloneInputModule>()) == null)
+            {
+                tempObj.AddComponent<StandaloneInputModule>();
+            }
+        }
+    }
+
+}
